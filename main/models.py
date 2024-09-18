f1a75dba3c7667bc64c018b1eb90d3b0b5db337c
Malvin <malvinmraqin@gmail.com>
2024-09-18 15:36:53 +0700

    fix github

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 import uuid  # tambahkan baris ini di paling atas
 
+
 class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
